09d3c5f208fc45b85d25bf369ee39d0bba7236b7
fixmypcn <fixmypcn@gmail.com>
2026-04-30 20:42:43 +0200

    update firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 window.CHECKLIST_FIREBASE_CONFIG = {
   enabled: false,
   firebase: {
-    apiKey: "PASTE_FIREBASE_API_KEY",
-    authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-    projectId: "PASTE_PROJECT_ID",
-    storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-    messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-    appId: "PASTE_APP_ID"
+    apiKey: "AIzaSyC_Fk31U_ObqhE9Ze3v2ed2B_MNyj2wZoY",
+    authDomain: "webapp-checklist.firebaseapp.com",
+    projectId: "webapp-checklist",
+    storageBucket: "webapp-checklist.firebasestorage.app",
+    messagingSenderId: "73116382577",
+    appId: "1:73116382577:web:bdd3bcb3b509c2a0e972d0",
+    measurementId: "G-N561QYNZXG"
   }
 };
